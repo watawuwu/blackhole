4c414983b5f40950f8743d4bd548f2d27a7e7226
Wataru Matsui <watawuwu@3bi.tech>
2019-08-20 17:51:37 +0900

    Change hyper to warp

---
+++ src/access_log.rs
@@ -0,0 +1,104 @@
+use ansi_term::Style;
+use ansi_term::{ANSIGenericString, Colour};
+use chrono::prelude::*;
+
+use serde_json::{json, Value};
+use std::collections::HashMap;
+use std::net::SocketAddr;
+
+pub struct AccessLog {
+    path: String,
+    query: HashMap<String, String>,
+    addr: Option<SocketAddr>,
+    body: String,
+    headers: HashMap<String, String>,
+    content_type: String,
+    method: String,
+    ts: DateTime<Local>,
+}
+
+impl AccessLog {
+    pub fn new(
+        method: String,
+        path: String,
+        query: HashMap<String, String>,
+        addr: Option<SocketAddr>,
+        body: String,
+        headers: HashMap<String, String>,
+    ) -> AccessLog {
+        let content_type = headers
+            .get("content-type")
+            .map(String::from)
+            .unwrap_or_default();
+
+        let ts = Local::now();
+
+        AccessLog {
+            path,
+            query,
+            addr,
+            headers,
+            body,
+            content_type,
+            method,
+            ts,
+        }
+    }
+
+    pub fn println(&self, max_chars: u32) {
+        let body = if self.is_logging() {
+            let buf = self.body.as_str();
+            let v: Value = serde_json::from_str(buf).unwrap_or_else(|_| json!(buf));
+            v.to_string()
+                .chars()
+                .take(max_chars as usize)
+                .collect::<String>()
+        } else {
+            String::from(r#""""#)
+        };
+        println!(
+            r#"{{{method_label}: {method_value}, {path_label}: {path_value}, {query_label}: {query_value}, {addr_label}: {addr_value}, {headers_label}: {headers_value}, {body_label}: {body_value}, {ts_label}: {ts_value}}}"#,
+            method_label = label("method"),
+            method_value = value(self.method.as_str()),
+            path_label = label("path"),
+            path_value = value(self.path.as_str()),
+            query_label = label("query"),
+            query_value = Style::new().fg(Colour::Green).paint(format!(r#"{:?}"#, self.query)),
+            addr_label = label("addr"),
+            addr_value = value(self.addr.map(|s|s.to_string()).unwrap_or_default().as_str()),
+            headers_label = label("headers"),
+            headers_value = Style::new().fg(Colour::Green).paint(format!(r#"{:?}"#, self.headers)),
+            body_label = label("body"),
+            body_value = Style::new().fg(Colour::Green).paint(body),
+            ts_label = label("ts"),
+            ts_value = value(self.rfc3339().as_str()),
+        );
+    }
+
+    fn rfc3339(&self) -> String {
+        self.ts.to_rfc3339_opts(SecondsFormat::Secs, true)
+    }
+
+    fn is_logging(&self) -> bool {
+        self.content_type.contains("text/")
+            || self.content_type.contains("application/json")
+            || self.content_type.contains("application/xml")
+            || self
+                .content_type
+                .contains("application/x-www-form-urlencoded")
+    }
+}
+
+fn label(input: &str) -> ANSIGenericString<str> {
+    Style::new()
+        .bold()
+        .fg(Colour::Blue)
+        .paint(format!(r#""{}""#, input))
+}
+
+fn value(input: &str) -> ANSIGenericString<str> {
+    let json: Value = json!(input);
+    Style::new()
+        .fg(Colour::Green)
+        .paint(format!(r#""{}""#, json.as_str().unwrap_or_default()))
+}
---
+++ src/server.rs
@@ -0,0 +1,131 @@
+use crate::args::Args;
+use crate::error::Result;
+use std::env;
+
+use crate::access_log::AccessLog;
+use log::*;
+use std::collections::HashMap;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use warp::body::FullBody;
+
+use warp::filters::path::FullPath;
+use warp::http::{HeaderMap, Method};
+use warp::{self, Buf, Filter};
+
+fn blackhole(
+    mac_chars: u32,
+    method: Method,
+    path: FullPath,
+    query: HashMap<String, String>,
+    addr: Option<SocketAddr>,
+    body: FullBody,
+    headers: HeaderMap,
+) -> impl warp::Reply {
+    let header_map = headers
+        .iter()
+        .map(|(h, v)| (h.as_str().to_string(), v.to_str().unwrap_or("").to_string()))
+        .collect::<HashMap<_, _>>();
+
+    let access_log = Arc::new(AccessLog::new(
+        method.to_string(),
+        path.as_str().into(),
+        query,
+        addr,
+        String::from_utf8_lossy(&body.bytes().to_vec()).to_string(),
+        header_map,
+    ));
+    access_log.clone().println(mac_chars);
+    warp::reply()
+}
+fn service(
+    max_chars: u32,
+) -> impl Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Copy {
+    let _a = warp::log::custom(|_info| println!("dddd"));
+
+    warp::any()
+        .and(warp::any().map(move || max_chars))
+        .and(warp::method())
+        .and(warp::path::full())
+        .and(warp::query::<HashMap<String, String>>())
+        .and(warp::addr::remote())
+        .and(warp::body::concat())
+        .and(warp::header::headers_cloned())
+        .map(blackhole)
+}
+
+pub fn serve(row_args: Vec<String>) -> Result<()> {
+    let args = Args::new(&row_args)?;
+
+    if let Some(level) = args.log_level() {
+        env::set_var("RUST_LOG", level);
+    }
+    pretty_env_logger::init();
+
+    debug!("start!");
+
+    let socket = args.socket_addr();
+    let max_chars = args.max_chars;
+
+    let service = service(max_chars);
+    warp::serve(service).run(socket);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn matches() {
+        let filter = service(1024);
+
+        let res = warp::test::request().path("/").reply(&filter);
+        assert_eq!(res.status(), 200);
+
+        let res = warp::test::request().path("/1/2").reply(&filter);
+        assert_eq!(res.status(), 200);
+
+        let res = warp::test::request().path("/?aaa=bbb").reply(&filter);
+        assert_eq!(res.status(), 200);
+
+        let res = warp::test::request()
+            .method("POST")
+            .body("aaa=bbb")
+            .path("/post")
+            .reply(&filter);
+        assert_eq!(res.status(), 200);
+
+        let res = warp::test::request().method("DELETE").reply(&filter);
+        assert_eq!(res.status(), 200);
+
+        let res = warp::test::request().method("PUT").reply(&filter);
+        assert_eq!(res.status(), 200);
+
+        let res = warp::test::request().method("OPTIONS").reply(&filter);
+        assert_eq!(res.status(), 200);
+
+        let res = warp::test::request().method("HEAD").reply(&filter);
+        assert_eq!(res.status(), 200);
+
+        let res = warp::test::request().method("TRANCE").reply(&filter);
+        assert_eq!(res.status(), 200);
+
+        let headers = vec![
+            ("Content-Type", "application/json"),
+            ("Content-Type", "application/xml"),
+            ("Content-Type", "application/x-www-form-urlencoded"),
+            ("Content-Type", "application/octet-stream"),
+            ("Content-Type", "application/pdf"),
+            ("Content-Type", "image/gif"),
+            ("Content-Type", "image/jpg"),
+            ("Content-Type", "image/png"),
+        ];
+        headers.into_iter().for_each(|(h, v)| {
+            let res = warp::test::request().header(h, v).reply(&filter);
+            assert_eq!(res.status(), 200);
+        });
+    }
+}
